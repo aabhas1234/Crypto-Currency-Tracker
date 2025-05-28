663b2c5adc999ff6e22518bd91b10adf7e294bad
aabhas1234 <136249872+aabhas1234@users.noreply.github.com>
2025-05-28 17:39:16 +0530

    28-05-2025-2

--- src/trending_coins.jsx
+++ src/trending_coins.jsx
@@ -12,8 +12,8 @@ const trending_coins = ({ Trending_Data }) => {
                 <div className='space-y-3 p-3'>
                     {
                         Trending_Data.slice(0, 3).map((value) => (<div className='space-x-5 
-              sm:space-x-20  sm:flex-nowrap flex text-left  '>
-                            <div className='flex justify-center  items-centre '>
+                sm:flex-nowrap flex '>
+                            <div className='flex justify-center  items-centre p-1 '>
                                 <img src={value.item.small} className='rounded-full h-[1rem] w-[1rem] my-auto'></img>
                                 <div className='flex flex-wrap p-1'>
                                     <h1 className='text-[0.3rem] sm:text-[0.6rem] '>{value.item.name}</h1>
@@ -22,45 +22,36 @@ const trending_coins = ({ Trending_Data }) => {
 
                             </div>
 
-                            <div className={classNames({
-                                'bg-red-50': value.item.data.price_change_percentage_24h.usd < 0,
-                                'bg-emerald-50': value.item.data.price_change_percentage_24h.usd > 0,
-                                'flex': true,
-                                'items-center': true,
-                                'justify-center': true,
-                                'text-[0.3rem]': true,
-                                'sm:text-[0.6rem]': true,
-                                'rounded-md': true,
-                                'w-7': true,
-                                'h-2': true,
-                                'sm:w-10': true,
-                                'sm:h-4': true,
-                                'text-red-400': value.item.data.price_change_percentage_24h.usd < 0,
-                                'text-emerald-400': value.item.data.price_change_percentage_24h.usd > 0,
-                                'text-left': true,
-                            })}
-
+                            <div
+                                className={classNames({
+                                    'flex items-center justify-center': true,
+                                    'rounded-sm my-auto': true,
+                                    'text-[0.3rem] sm:text-[0.6rem]': true,
+                                    'h-3 sm:h-4 sm:w-10': true,
+                                    'bg-red-50 text-red-400': value.item.data.price_change_percentage_24h.usd < 0,
+                                    'bg-emerald-50 text-emerald-400': value.item.data.price_change_percentage_24h.usd > 0,
+                                })}
                             >
-                                {value.item.data.price_change_percentage_24h.usd < 0 ? <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="10.033"
-                                    height="5"
-                                    viewBox="0 0 10.033 5"
-                                    class="fill-current text-red-400 mr-1"
-                                >
-                                    <polygon points="5.016 0 0 0.003 2.506 2.5 5.016 5 7.525 2.5 10.033 0.003 5.016 0" />
-                                </svg> : <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="10.033"
-                                    height="5"
-                                    viewBox="0 0 10.033 5"
-                                    class="fill-current text-emerald-400 mr-1"
-                                >
-                                    <polygon points="5.016 0 2.506 2.5 0 4.999 5.016 5 10.033 4.999 7.525 2.5 5.016 0" />
-                                </svg>
-                                }
+                                {value.item.data.price_change_percentage_24h.usd < 0 ? (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 10.033 5"
+                                        className="w-[10px] h-[5px] fill-current text-red-400 mr-1 shrink-0"
+                                    >
+                                        <polygon points="5.016 0 0 0.003 2.506 2.5 5.016 5 7.525 2.5 10.033 0.003 5.016 0" />
+                                    </svg>
+                                ) : (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 10.033 5"
+                                        className="w-[10px] h-[5px] fill-current text-emerald-400 mr-1 shrink-0"
+                                    >
+                                        <polygon points="5.016 0 2.506 2.5 0 4.999 5.016 5 10.033 4.999 7.525 2.5 5.016 0" />
+                                    </svg>
+                                )}
                                 {parseFloat(Number(value.item.data.price_change_percentage_24h.usd).toFixed(2))}%
                             </div>
+
                         </div>))
                     }
 
